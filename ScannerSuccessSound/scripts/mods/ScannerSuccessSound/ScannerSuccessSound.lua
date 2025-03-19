f49c5b4988efc3efddaafeb9d2dbd4c20fc5e198
Backup158 <beekoop158@gmail.com>
2025-03-19 04:07:21 -0500

    Added widget functionality

--- ScannerSuccessSound/scripts/mods/ScannerSuccessSound/ScannerSuccessSound.lua
+++ ScannerSuccessSound/scripts/mods/ScannerSuccessSound/ScannerSuccessSound.lua
@@ -7,20 +7,35 @@ mod.version = "1.0"
 local PlayerCharacterSoundEventAliases = require("scripts/settings/sound/player_character_sound_event_aliases")
 
 --#################################
--- Hooks
+-- Helper Functions
+--#################################
+local function replaceTheSound()
+    local debug = mod:get("enable_debug_mode")
+    local replacementSound = mod:get("scan_sound")
+    local replacementTable = {}
+    -- Splits value into keys 
+    --  %. escapes the magic character (period)
+    --  [^%.] match anything that's not a period
+    --  [^%.]+ match the longest string of not periods
+    for v in string.gmatch(replacementSound, "[^%.]+") do 
+        table.insert(replacementTable, v)
+        if debug then mod:echo("Split string result: "..tostring(v)) end
+    end
+
+    if debug then 
+        mod:echo("Replacement Sound is: "..replacementSound)
+        mod:echo("Replacing sfx_scanning_sucess.events.scanner_equip with: "..PlayerCharacterSoundEventAliases[replacementTable[1]][replacementTable[2]][replacementTable[3]]) 
+    end
+    PlayerCharacterSoundEventAliases.sfx_scanning_sucess.events.scanner_equip = PlayerCharacterSoundEventAliases[replacementTable[1]][replacementTable[2]][replacementTable[3]]
+end
+
+--#################################
+-- Hooks and Execution
 --#################################
 mod.on_all_mods_loaded = function()
     mod:info("ScannerSuccessSound v" .. mod.version .. " loaded uwu nya :3")
-    local debug = mod:get("enable_debug_mode")
-    local replacementSound = mod:get("scan_sound")
-
-    if debug then mod:echo("Replacement Sound is: "..replacementSound) end
-    --PlayerCharacterSoundEventAliases.sfx_minigame_success.events.auspex_scanner = PlayerCharacterSoundEventAliases.ability_shout.events.veteran_combat_ability
-    if debug then mod:echo("Replacing sfx_scanning_sucess.events.scanner_equip with: "..PlayerCharacterSoundEventAliases[ability_shout.events.veteran_combat_ability]) end
-    PlayerCharacterSoundEventAliases.sfx_scanning_sucess.events.scanner_equip = PlayerCharacterSoundEventAliases[ability_shout.events.veteran_combat_ability]
-    
-    
-    --"wwise/events/player/play_device_auspex_scanner_minigame_progress"
-
-
+    replaceTheSound()
+end
+mod.on_setting_changed = function()
+    replaceTheSound()
 end
